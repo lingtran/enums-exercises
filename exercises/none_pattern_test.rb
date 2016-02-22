ceeb322b1e1af963212e425c8cbd14c86d0a0156
lingtran <lxiutran@gmail.com>
2016-02-21 20:33:08 -0700

    initial commit for enums-exercises

--- exercises/none_pattern_test.rb
+++ exercises/none_pattern_test.rb
@@ -17,37 +17,44 @@ class NonePatternTest < Minitest::Test
     numbers = [9, 3, 3, 7, 6, -5, 1]
     not_none_negative = true
     numbers.each do |number|
-      # Your code goes here
+     not_none_negative = false unless number.negative?
     end
     refute not_none_negative
   end
 
   def test_none_are_negative
-    skip
     numbers = [9, 3, 1, 8, 3, 3, 5]
     none_negative = true
-    # Your code goes here
+    numbers.each do |num|
+      none_negative = false unless num.positive?
+    end
     assert none_negative
   end
 
   def test_none_shall_pass
-    skip
     critters = ["elf", "hobbit", "dwarf", "wizard", "human"]
-    # Your code goes here
+    none_shall_pass = true
+    critters.each do |critter|
+      none_shall_pass = false unless critters.include?(critter)
+    end
     assert none_shall_pass
   end
 
   def test_one_or_more_shall_pass
-    skip
     phrases = ["go", "left", "can cross", "shall pass", "must stay", "tarried"]
-    # Your code goes here
+    none_shall_pass = true
+    phrases.each do |phrase|
+      none_shall_pass = false unless phrase == "shall pass"
+    end
     refute none_shall_pass
   end
 
   def test_none_even
-    skip
     numbers = [3, 9, 15, 21, 19]
-    # Your code goes here
+    none_even = true
+    numbers.each do |num|
+      none_even = false unless num.odd?
+    end
     assert none_even
   end
 
